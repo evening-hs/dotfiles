ef54af6f2f7735f30eaf7abcde9dae04c9e15f6a
Huerta Saavedra <gasparhs400@gmail.com>
2023-11-17 21:51:12 -0600

    Starting my own qtile config
    I will be adding some issues on this config
    
    In this commit I only moved the bar to a more GNOME-like style
    
    Things I want to add:
    
    - Choose a colorscheme
    - Change the pomodoro icon
    - Change pomodoro background according to time
    - Change the battery widget so it shows remaining time instead of
      battery percentage on click.
    - Change the battery widget so it changes the power profile on secondary
      click
    - Make the hour widget open tty-clock on click
    - Make the date widget open cal on click
    - Create a spotify widget

--- qtile/modules/screens.py
+++ qtile/modules/screens.py
@@ -4,104 +4,74 @@ from libqtile.config import Screen
 from modules.keys import terminal
 import os
 
-default_bar = bar.Bar(
-            [   
-                widget.Sep(padding=3, linewidth=0, background="#2f343f"),
-                
-                widget.Image(
-                    filename='~/.config/qtile/eos-c.png',
-                    margin=3,
-                    background="#2f343f",
-                    mouse_callbacks={'Button1': lambda: qtile.cmd_spawn("rofi -show combi")}),
-
-                widget.Sep(padding=4, linewidth=0, background="#2f343f"), 
-
+simple_bar = bar.Bar([
                 widget.GroupBox(
-                                highlight_method='line',
-                                this_screen_border="#5294e2",
-                                this_current_screen_border="#5294e2",
-                                active="#ffffff",
-                                inactive="#848e96",
-                                background="#2f343f"),
-
-                widget.TextBox(
-                       text = '',
-                       padding = 0,
-                       fontsize = 28,
-                       foreground='#2f343f'
-                       ),    
-                widget.Prompt(),
-                widget.Spacer(length=5),
-                widget.WindowName(foreground='#99c0de',fmt='{}'),
-                widget.Chord(
-                    chords_colors={
-                        'launch': ("#ff0000", "#ffffff"),
-                    },
-                    name_transform=lambda name: name.upper(),
+                    highlight_method='line',
+                    this_screen_border="#5294e2",
+                    this_current_screen_border="#5294e2",
+                    active="#ffffff",
+                    inactive="#848e96",
+                    background="#2f343f"
                 ),
-                widget.CurrentLayoutIcon(scale=0.75),
-                widget.CheckUpdates(
-                    update_interval=1800,
-                    distro="Arch_yay",
-                    display_format="{updates} Updates",
-                    foreground="#ffffff",
-                    mouse_callbacks={
-                        'Button1':
-                        lambda: qtile.cmd_spawn(terminal + ' -e yay -Syu')
-                    },
-                    background="#2f343f"),
-                widget.Systray(icon_size = 20),
-                widget.TextBox(
-                       text = '',
-                       padding = 0,
-                       fontsize = 28,
-                       foreground='#2f343f'
-                       ), 
-                volume,
-                widget.TextBox( 
-                       text = '',
-                       padding = 0,
-                       fontsize = 28,
-                       foreground='#2f343f',
-                       ),   
-                widget.TextBox(
-                       text = '',
-                       padding = 0,
-                       fontsize = 28,
-                       foreground='#2f343f'
-                       ),    
-                widget.Clock(format='󰥔 %Y-%m-%d %a %I:%M %p',
-                             background="#2f343f",
-                             foreground='#9bd689'),
-                                                widget.TextBox(                                                
-                                                
-                       text = '',
-                       padding = 0,
-                       fontsize = 28,
-                       foreground='#2f343f',
-                       ),
-                pomodoro,
-                widget.Battery(
-                        format = '{percent:0.0%}',
-                        low_percentage = 0.2,
-                    ),
-                widget.TextBox(
-                    text='',
-                    mouse_callbacks= {
-                        'Button1':
-                        lambda: qtile.cmd_spawn(os.path.expanduser('~/.config/rofi/powermenu.sh'))
-                    },
-                    foreground='#e39378'
+    ],
+        30,  # height in px
+        background="#404552",
+        opacity=0.75,
+        margin=6,
+    )
+
+default_bar = bar.Bar(
+        [
+            widget.GroupBox(
+                active="#ffffff",
+                background='#131313',
+                dissable_drag=True,
+                highlight_method='block',
+                inactive='#ffffff',
+                this_current_screen_border="#ff0000",
+                ),
+            widget.Spacer(),
+            widget.Sep(size_percent=1),
+            # calendar
+            widget.Clock(
+                background="#131313",
+                format="%b %d"
+                ),
+            # clock
+            widget.Clock(
+                background="#131313"
+                ),
+            widget.Spacer(),
+            widget.WidgetBox(
+                widgets=[
+                    widget.Systray(),
+                    ]
+                ),
+            widget.Sep(size_percent=1),
+            MyPomodoro(
+                background="#262626",
+                color_active="#ffffff",
+                color_inactive="#ffffff",
+                color_break="#ff9999",
+                length_long_break=5,
+                length_short_break=2,
+                length_pomodori=15,
+                prefix_inactive='\ue001'
+                ),
+            widget.Sep(size_percent=1),
+            widget.Battery(
+                background="#131313",
+                format = '{percent:2.0%}',
+                low_background="#ff9999"
                 )
-                
-            ],
-            30,  # height in px
-            background="#404552",
-            opacity=0.75,
-            margin=6,
-        )
+        ],
+        30,  # height in px
+        background="#00000000",
+        opacity=1,
+        margin=6,
+    )
 
 screens = [
-    Screen(bottom=default_bar,wallpaper="~/.local/share/backgrounds/lighthouse.jpg",wallpaper_mode="fill"),
     Screen(top=default_bar,wallpaper="~/.local/share/backgrounds/lighthouse.jpg",wallpaper_mode="fill"),
+    Screen(bottom=simple_bar,wallpaper="~/.local/share/backgrounds/lighthouse.jpg",wallpaper_mode="fill"),
 ]
