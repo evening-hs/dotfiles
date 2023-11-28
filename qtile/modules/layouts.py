09f78e71c248d1dd57998a3d0e607c3fd67e5f71
Huerta Saavedra <gasparhs400@gmail.com>
2023-11-27 21:08:50 -0600

    Fixed a bug when floating windows on a not floating layout wouldn't show the default colors

--- qtile/modules/layouts.py
+++ qtile/modules/layouts.py
@@ -8,41 +8,48 @@ border_width = 4
 
 layouts = [
     layout.Bsp(
-        margin=6,
-        border_focus=active_border_color,
-        border_normal=inactive_border_color,
-        fair=False,
-        border_on_single=True,
-        margin_on_single=[30, 60, 30, 60],
-        border_width = border_width
+        margin          = 6,
+        border_focus    = active_border_color,
+        border_normal   = inactive_border_color,
+        fair            = False,
+        border_on_single= True,
+        margin_on_single= [30, 60, 30, 60],
+        border_width    = border_width
     ),
 
     layout.Max(
-        margin=8,
-        border_focus=active_border_color,
-        border_normal=inactive_border_color,
-        border_width=border_width
+        margin          = 8,
+        border_focus    = active_border_color,
+        border_normal   = inactive_border_color,
+        border_width    = border_width
     ),
 
     layout.Floating(
-        border_normal=active_border_color,
-        border_focus=inactive_border_color,
-        border_width=border_width,
-        full_screen_border_width=border_width,
-        max_border_width=border_width
+        border_normal           = inactive_border_color,
+        border_focus            = active_border_color,
+        border_width            = border_width,
+        fullscreen_border_width = border_width,
+        max_border_width        = border_width
     ),
 ]
 
-floating_layout = layout.Floating(float_rules=[
-    # Run the utility of `xprop` to see the wm class and name of an X client.
-    *layout.Floating.default_float_rules,
-    Match(wm_class='confirmreset'),  # gitk
-    Match(wm_class='makebranch'),  # gitk
-    Match(wm_class='maketag'),  # gitk
-    Match(wm_class='ssh-askpass'),  # ssh-askpass
-    Match(title='branchdialog'),  # gitk
-    Match(title='pinentry'),  # GPG key password entry
-    Match(title='Screenshot'),
-    Match(title='Pomodoro'),
-    Match(title='Qalculate!'),
-])
+floating_layout = layout.Floating(
+        border_normal           = inactive_border_color,
+        border_focus            = active_border_color,
+        border_width            = border_width,
+        fullscreen_border_width = border_width,
+        max_border_width        = border_width,
+        float_rules=[
+        # Run the utility of `xprop` to see the wm class and name of an X client.
+        *layout.Floating.default_float_rules,
+        Match(wm_class='confirmreset'),  # gitk
+        Match(wm_class='makebranch'),  # gitk
+        Match(wm_class='maketag'),  # gitk
+        Match(wm_class='ssh-askpass'),  # ssh-askpass
+        Match(title='branchdialog'),  # gitk
+        Match(title='pinentry'),  # GPG key password entry
+        Match(title='Screenshot'),
+        Match(title='Pomodoro'),
+        Match(title='Qalculate!'),
+    ]
+    )
